17caec5375a38b16c0f638f3bda70f1c018545e9
nikodev <nikolas090189@gmail.com>
2026-01-22 03:12:40 +0000

    refactor: improve port handling in bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,8 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port, '0.0.0.0');
   console.log(`App running on port: ${process.env.PORT}`);
 }
 void bootstrap();
